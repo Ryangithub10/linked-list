e6392e088ee167aa9d3c9e571398dabc495606e4
Ryangithub10 <technogamers543@gmail.com>
2025-02-27 15:37:07 +0800

    making documentation (not complete)

--- list.h
+++ list.h
@@ -3,6 +3,44 @@
 
 #include <stdbool.h>
 
+/** Linked List Documentation
+ *
+ * == Initialize
+ * newNode(int data) -> construct new node
+ * @param data -> specify data
+ * @return -> Pointer of Node
+ *
+ * newList() -> construct new Linked List
+ * @return Pointer of LinkedList
+ *
+ * == Searching / Accessing
+ * search(LinkedList* list, int target) -> search node by value
+ * @param list -> specify linked list pointer
+ * @param target
+ * @return Node pointer
+ *
+ * at(LinkedList* list, int index) -> search node by index
+ * @param list -> specify linked list pointer
+ * @param target
+ * @return Node pointer
+ *
+ * == Insertion
+ * add(LinkedList* list, int newData) -> add new node at beginning
+ * @param list -> specify linked list pointer 
+ * @param newData -> specify data for new node
+ *
+ * push(LinkedList* list, int newData) -> add new node at end
+ * @param list -> specify linked list pointer
+ * @param newData -> specify data for new node
+ *
+ * insert(LinkedList* list, int index, int newData) -> add new node at specify location 
+ * @param list -> specify linked list pointer
+ * @param index -> specify new node location
+ * @param newData -> specify data for new node
+ *
+ */
+
+
 // Node 
 typedef struct Node {
 	int data;
